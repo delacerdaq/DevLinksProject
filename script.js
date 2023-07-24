b08635a989a57509da71de877b34b261491f76ca
Ana Luiza Lacerda <analuizadelacerda@gmail.com>
2023-07-24 10:52:17 -0300

    altering img description

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ function toggleMode(){
     if(html.classList.contains('light'))
     { //se tiver light mode, adicionar a imagem light
        img.setAttribute('src', '/assets/avatar-light.png') 
+       img.alt = "Foto de Mayk Brito sorrindo, usando óculos escuros e camisa preta, barba e fundo degradê azul e roxo "
     } 
     else
     { //se tiver sem light mode, manter a imagem normal
         img.setAttribute('src', '/assets/avatar.png') 
+        img.alt = "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo degradê azul e roxo "
     }
     
 
